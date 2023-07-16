44aadef2c0e6df09dcc632d2c674c25787c162e2
Martin Arroyo <marroyo7775@gmail.com>
2023-07-16 15:01:41 -0400

    Changed paths to URLS in data load func

--- src/teachdb/teachdb.py
+++ src/teachdb/teachdb.py
@@ -1,12 +1,21 @@
 import duckdb
 import pandas as pd
-from pathlib import Path
+
 
 def download_db():
     """Returns a dictionary of dataframes as raw data"""
-    raw_data_loc = Path.cwd().glob("data/*.csv")
+    raw_data_loc = [
+        {"table": "salesman", "path": "https://raw.githubusercontent.com/freestackinitiative/coop_sql_notebooks/main/data/SALESMAN.csv"},
+        {"table": "customer", "path":"https://raw.githubusercontent.com/freestackinitiative/coop_sql_notebooks/main/data/CUSTOMER.csv"},
+        {"table": "order_details", "path":"https://raw.githubusercontent.com/freestackinitiative/coop_sql_notebooks/main/data/ORDERDETAILS.csv"},
+        {"table": "foods", "path": "https://raw.githubusercontent.com/freestackinitiative/coop_sql_notebooks/main/data/foods.csv"},
+        {"table": "company", "path": "https://raw.githubusercontent.com/freestackinitiative/coop_sql_notebooks/main/data/company.csv"},
+        {"table": "movies", "path": "https://raw.githubusercontent.com/freestackinitiative/coop_sql_notebooks/main/data/movies.csv"},
+        {"table": "boxoffice", "path": "https://raw.githubusercontent.com/freestackinitiative/coop_sql_notebooks/main/data/boxoffice.csv"},
+        {"table": "employees", "path": "https://raw.githubusercontent.com/freestackinitiative/coop_sql_notebooks/main/data/employees.csv"}
+    ]
     raw_data = {}
     for data in raw_data_loc:
-        raw_data[data.stem] = pd.read_csv(data)
+        raw_data[data['table']] = pd.read_csv(data['path'])
     
     return raw_data
