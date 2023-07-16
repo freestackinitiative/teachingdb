1758ca12c8a37a9d8c1753cd4ed42e8e2065357c
Martin Arroyo <marroyo7775@gmail.com>
2023-07-16 14:43:10 -0400

    Updated connect_to_db to take a connection as input

--- src/teachdb/teachdb.py
+++ src/teachdb/teachdb.py
@@ -2,9 +2,8 @@ import duckdb
 import pandas as pd
 from pathlib import Path
 
-def connect_to_db():
+def connect_to_db(con):
     """Reads in CSV data, creates and returns a DuckDB connection for an in-memory database."""
-    con = duckdb.connect(":memory:")
     raw_data_loc = Path.cwd().glob("data/*.csv")
     for data in raw_data_loc:
         df = pd.read_csv(data)
