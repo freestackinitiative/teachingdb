4c45d35e59d841ecd81c280f5a8a7cdce1982b45
Martin Arroyo <marroyo7775@gmail.com>
2023-07-20 22:28:44 -0400

    Troubleshooting

--- src/teachdb/teachdb.py
+++ src/teachdb/teachdb.py
@@ -1,6 +1,6 @@
 import duckdb
 import pandas as pd
-from teachdb.loader import load_paths
+from teachdb.loader import load_paths, loader
 
 def download_db(paths):
     """Returns a dictionary of dataframes as raw data"""
@@ -21,3 +21,7 @@ def connect_teachdb(con, database="core"):
     raw_data = download_db(paths=paths)
     connect_db(con, raw_data)
     print(f"Connected to the `teachdb` from the Freestack Initiative. You are using the `{database}` database.")
+
+
+def tester():
+    loader()
