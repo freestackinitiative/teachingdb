465ceb6187e3ff1fd0fa6a9e93283ec24380cc90
Martin Arroyo <marroyo7775@gmail.com>
2024-06-30 20:29:42 -0400

    Added INTEGER to autodetect list

--- src/teachdb/teachdb.py
+++ src/teachdb/teachdb.py
@@ -24,7 +24,7 @@ def _connect_db(con: duckdb.DuckDBPyConnection,
     for table_name, table_data in schema.items():
         con.sql(f"CREATE TABLE {table_name} AS "
                 f"SELECT * FROM read_csv('{table_data}', "
-                "auto_type_candidates = [DATE, TIMESTAMP])")
+                "auto_type_candidates = [DATE, TIMESTAMP, INTEGER])")
     return con
 
 
